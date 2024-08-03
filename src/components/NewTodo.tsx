847eaa87db6773d9d1326ab9b89bcf7e6de11890
Hansana Sandakelum <95872422+HansanaSandakelum@users.noreply.github.com>
2024-08-03 18:09:49 +0530

    Add NewTodo component and update App.tsx to manage todos

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 
-const NewTodo = () => {
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
@@ -10,13 +10,14 @@ const NewTodo = () => {
 
     if (enteredText.trim().length === 0) {
       // throw an error
-      return
-        
+      return;
     }
+
+    props.onAddTodo?.(enteredText);
   };
   return (
     <form onSubmit={submitHandler}>
-      <label htmlFor="text"></label>
+      <label htmlFor="text">Todo text</label>
       <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
